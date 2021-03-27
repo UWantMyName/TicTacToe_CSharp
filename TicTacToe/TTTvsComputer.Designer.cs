01a6703c9ae0cee32acee423d8327fa05cdfdd5c
Soumya <satishsrinivasan0@gmail.com>
2021-03-27 12:06:58 +0000

    Changed TTTvsComputer. Doesn't work.

--- TicTacToe/TTTvsComputer.Designer.cs
+++ TicTacToe/TTTvsComputer.Designer.cs
@@ -29,19 +29,194 @@ namespace TicTacToe
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.CC = new System.Windows.Forms.Button();
+			this.CB = new System.Windows.Forms.Button();
+			this.CA = new System.Windows.Forms.Button();
+			this.BC = new System.Windows.Forms.Button();
+			this.BB = new System.Windows.Forms.Button();
+			this.BA = new System.Windows.Forms.Button();
+			this.AC = new System.Windows.Forms.Button();
+			this.AB = new System.Windows.Forms.Button();
+			this.AA = new System.Windows.Forms.Button();
 			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.Color.White;
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(15, 14);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(493, 50);
+			this.textBox1.TabIndex = 22;
+			this.textBox1.Text = "Tic Tac Toe";
+			this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// CC
+			// 
+			this.CC.BackColor = System.Drawing.Color.White;
+			this.CC.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.CC.Font = new System.Drawing.Font("Microsoft Sans Serif", 1.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.CC.ForeColor = System.Drawing.Color.DimGray;
+			this.CC.Location = new System.Drawing.Point(351, 392);
+			this.CC.Margin = new System.Windows.Forms.Padding(4);
+			this.CC.Name = "CC";
+			this.CC.Size = new System.Drawing.Size(160, 148);
+			this.CC.TabIndex = 21;
+			this.CC.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+			this.CC.UseVisualStyleBackColor = false;
+			// 
+			// CB
+			// 
+			this.CB.BackColor = System.Drawing.Color.White;
+			this.CB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.CB.Font = new System.Drawing.Font("Microsoft Sans Serif", 1.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.CB.ForeColor = System.Drawing.Color.DimGray;
+			this.CB.Location = new System.Drawing.Point(183, 392);
+			this.CB.Margin = new System.Windows.Forms.Padding(4);
+			this.CB.Name = "CB";
+			this.CB.Size = new System.Drawing.Size(160, 148);
+			this.CB.TabIndex = 20;
+			this.CB.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+			this.CB.UseVisualStyleBackColor = false;
+			// 
+			// CA
+			// 
+			this.CA.BackColor = System.Drawing.Color.White;
+			this.CA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.CA.Font = new System.Drawing.Font("Microsoft Sans Serif", 1.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.CA.ForeColor = System.Drawing.Color.DimGray;
+			this.CA.Location = new System.Drawing.Point(15, 392);
+			this.CA.Margin = new System.Windows.Forms.Padding(4);
+			this.CA.Name = "CA";
+			this.CA.Size = new System.Drawing.Size(160, 148);
+			this.CA.TabIndex = 19;
+			this.CA.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+			this.CA.UseVisualStyleBackColor = false;
+			// 
+			// BC
+			// 
+			this.BC.BackColor = System.Drawing.Color.White;
+			this.BC.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.BC.Font = new System.Drawing.Font("Microsoft Sans Serif", 1.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.BC.ForeColor = System.Drawing.Color.DimGray;
+			this.BC.Location = new System.Drawing.Point(350, 237);
+			this.BC.Margin = new System.Windows.Forms.Padding(4);
+			this.BC.Name = "BC";
+			this.BC.Size = new System.Drawing.Size(160, 148);
+			this.BC.TabIndex = 18;
+			this.BC.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+			this.BC.UseVisualStyleBackColor = false;
+			// 
+			// BB
+			// 
+			this.BB.BackColor = System.Drawing.Color.White;
+			this.BB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.BB.Font = new System.Drawing.Font("Microsoft Sans Serif", 1.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.BB.ForeColor = System.Drawing.Color.DimGray;
+			this.BB.Location = new System.Drawing.Point(183, 237);
+			this.BB.Margin = new System.Windows.Forms.Padding(4);
+			this.BB.Name = "BB";
+			this.BB.Size = new System.Drawing.Size(160, 148);
+			this.BB.TabIndex = 17;
+			this.BB.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+			this.BB.UseVisualStyleBackColor = false;
+			// 
+			// BA
+			// 
+			this.BA.BackColor = System.Drawing.Color.White;
+			this.BA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.BA.Font = new System.Drawing.Font("Microsoft Sans Serif", 1.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.BA.ForeColor = System.Drawing.Color.DimGray;
+			this.BA.Location = new System.Drawing.Point(15, 237);
+			this.BA.Margin = new System.Windows.Forms.Padding(4);
+			this.BA.Name = "BA";
+			this.BA.Size = new System.Drawing.Size(160, 148);
+			this.BA.TabIndex = 16;
+			this.BA.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+			this.BA.UseVisualStyleBackColor = false;
+			// 
+			// AC
+			// 
+			this.AC.BackColor = System.Drawing.Color.White;
+			this.AC.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.AC.Font = new System.Drawing.Font("Microsoft Sans Serif", 1.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.AC.ForeColor = System.Drawing.Color.DimGray;
+			this.AC.Location = new System.Drawing.Point(351, 81);
+			this.AC.Margin = new System.Windows.Forms.Padding(4);
+			this.AC.Name = "AC";
+			this.AC.Size = new System.Drawing.Size(160, 148);
+			this.AC.TabIndex = 15;
+			this.AC.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+			this.AC.UseVisualStyleBackColor = false;
+			// 
+			// AB
+			// 
+			this.AB.BackColor = System.Drawing.Color.White;
+			this.AB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.AB.Font = new System.Drawing.Font("Microsoft Sans Serif", 1.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.AB.ForeColor = System.Drawing.Color.DimGray;
+			this.AB.Location = new System.Drawing.Point(183, 81);
+			this.AB.Margin = new System.Windows.Forms.Padding(4);
+			this.AB.Name = "AB";
+			this.AB.Size = new System.Drawing.Size(160, 148);
+			this.AB.TabIndex = 14;
+			this.AB.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+			this.AB.UseVisualStyleBackColor = false;
+			// 
+			// AA
+			// 
+			this.AA.BackColor = System.Drawing.Color.White;
+			this.AA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.AA.Font = new System.Drawing.Font("Microsoft Sans Serif", 1.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.AA.ForeColor = System.Drawing.Color.DimGray;
+			this.AA.Location = new System.Drawing.Point(15, 81);
+			this.AA.Margin = new System.Windows.Forms.Padding(4);
+			this.AA.Name = "AA";
+			this.AA.Size = new System.Drawing.Size(160, 148);
+			this.AA.TabIndex = 13;
+			this.AA.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+			this.AA.UseVisualStyleBackColor = false;
 			// 
 			// TTTvsComputer
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(374, 450);
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(527, 554);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.CC);
+			this.Controls.Add(this.CB);
+			this.Controls.Add(this.CA);
+			this.Controls.Add(this.BC);
+			this.Controls.Add(this.BB);
+			this.Controls.Add(this.BA);
+			this.Controls.Add(this.AC);
+			this.Controls.Add(this.AB);
+			this.Controls.Add(this.AA);
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.Name = "TTTvsComputer";
-			this.Text = "ComputerTicTacToeForm";
+			this.Text = "Tic Tac Toe";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button CC;
+		private System.Windows.Forms.Button CB;
+		private System.Windows.Forms.Button CA;
+		private System.Windows.Forms.Button BC;
+		private System.Windows.Forms.Button BB;
+		private System.Windows.Forms.Button BA;
+		private System.Windows.Forms.Button AC;
+		private System.Windows.Forms.Button AB;
+		private System.Windows.Forms.Button AA;
 	}
 }
--- TicTacToe/TTTvsComputer.cs
+++ TicTacToe/TTTvsComputer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,187 @@ namespace TicTacToe
 {
 	public partial class TTTvsComputer : Form
 	{
+		bool X = true;
+		string generalPath = Directory.GetCurrentDirectory();
+		string XPath;
+		string OPath;
+		string winner = "";
+		Button[,] Grid = new Button[3, 3];
 		public TTTvsComputer()
 		{
 			InitializeComponent();
+			for (int i = 0; i < generalPath.Length - 9; i++)
+			{
+				XPath += generalPath[i];
+				OPath += generalPath[i];
+			}
+
+			XPath += "X.png";
+			OPath += "0.png";
+
+
+			Grid[0, 0] = AA;
+			Grid[0, 1] = AB;
+			Grid[0, 2] = AC;
+
+			Grid[1, 0] = BA;
+			Grid[1, 1] = BB;
+			Grid[1, 2] = BC;
+
+			Grid[2, 0] = CA;
+			Grid[2, 1] = CB;
+			Grid[2, 2] = CC;
+		}
+
+		public void ButtonClick(object sender, EventArgs e)
+		{
+			Button b = sender as Button;
+
+			if (!b.Enabled || !X) return;
+
+			b.BackgroundImage = Image.FromFile(XPath);
+			b.Text = "X";
+			b.TextAlign = ContentAlignment.TopCenter;
+			
+
+			b.BackgroundImageLayout = ImageLayout.Stretch;
+
+
+			b.Enabled = false;
+			X = !X;
+
+			if (Check())
+			{
+				string message;
+
+				if (winner == "-")
+				{
+					message = "Game was a draw.";
+				}
+
+				else
+				{
+					message = "This game was won by " + winner + ".";
+				}
+
+				MessageBox.Show(message);
+				DisableAllButtons();
+			}
+
+			ComputerTurn();
+		}
+
+		public bool Check()
+		{
+			bool GameFinished = false;
+			string result = "";
+
+			// Horizontal
+
+			for (int j = 0; j < 3 && !GameFinished; j++)
+			{
+				result = "";
+
+				for (int i = 0; i < 3; i++)
+				{
+					result += Grid[i, j].Text;
+				}
+
+				if (result == "000" || result == "XXX")
+				{
+					GameFinished = true;
+					winner = result[0].ToString();
+				}
+			}
+
+			// Vertical
+
+			for (int j = 0; j < 3 && !GameFinished; j++)
+			{
+				result = "";
+
+				for (int i = 0; i < 3; i++)
+				{
+					result += Grid[j, i].Text;
+				}
+
+				if (result == "000" || result == "XXX")
+				{
+					GameFinished = true;
+					winner = result[0].ToString();
+				}
+			}
+
+			// Primary Diagonal
+			result = "";
+			result += Grid[0, 0].Text + Grid[1, 1].Text + Grid[2, 2].Text;
+
+
+			if (result == "000" || result == "XXX")
+			{
+				GameFinished = true;
+				winner = result[0].ToString();
+			}
+
+
+
+			// Secondary Diagonal
+			result = "";
+			result += Grid[0, 2].Text + Grid[1, 1].Text + Grid[2, 0].Text;
+
+
+			if (result == "000" || result == "XXX")
+			{
+				GameFinished = true;
+				winner = result[0].ToString();
+			}
+
+			// Checking for Draw
+
+			bool full = true;
+
+			for (int i = 0; i < 3; i++)
+				for (int j = 0; j < 3; j++)
+				{
+					if (Grid[i, j].Text == "") full = false;
+				}
+
+
+			if (!GameFinished && full)
+			{
+				GameFinished = true;
+				winner = "-";
+			}
+
+			return GameFinished;
+
+		}
+
+		private void ComputerTurn()
+		{
+			List<Button> enabled = new List<Button>();
+
+			for (int i = 0; i < 3; i++)
+				for (int j = 0; j < 3; j++)
+				{
+					if (Grid[i, j].Enabled) enabled.Add(Grid[i, j]);
+				}
+
+
+			Random rnd = new Random();
+			int index = rnd.Next(enabled.Count);
+
+			Button b = enabled[index];
+
+			b.BackgroundImage = Image.FromFile(OPath);
+			b.Text = "0";
+			b.TextAlign = ContentAlignment.MiddleCenter;
+		}
+		public void DisableAllButtons()
+		{
+			for (int i = 0; i < 3; i++)
+				for (int j = 0; j < 3; j++)
+					Grid[i, j].Enabled = false;
 		}
 	}
 }
